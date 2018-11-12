f979c124a64e3a0da9e2ad5df6eb1d852e8e1990
Manogna40 <43471477+Manogna40@users.noreply.github.com>
2018-11-12 23:51:30 +0530

    "check_style_score -> 0 of 8 passed. style score: 0/1 "

--- m1/1.1 Graph/Solution.java
+++ m1/1.1 Graph/Solution.java
@@ -1,6 +1,4 @@
-package module1;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +6,96 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
+/*************************************************************************
+ *  Compilation:  javac Bag.java
+ *  Execution:    java Bag < input.txt
+ *
+ *  A generic bag or multiset, implemented using a linked list.
+ *
+ *************************************************************************/
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ *  The <tt>Bag</tt> class represents a bag (or multiset) of 
+ *  generic items. It supports insertion and iterating over the 
+ *  items in arbitrary order.
+ *  <p>
+ *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
+ *  take constant time. Iteration takes time proportional to the number of items.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ */
+class Bag<Item> implements Iterable<Item> {
+    private int N;         // number of elements in bag
+    private Node first;    // beginning of bag
+
+    // helper linked list class
+    private class Node {
+        private Item item;
+        private Node next;
+    }
+
+   /**
+     * Create an empty stack.
+     */
+    public Bag() {
+        first = null;
+        N = 0;
+    }
+
+   /**
+     * Is the BAG empty?
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+   /**
+     * Return the number of items in the bag.
+     */
+    public int size() {
+        return N;
+    }
+
+   /**
+     * Add the item to the bag.
+     */
+    public void add(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+
+   /**
+     * Return an iterator that iterates over the items in the bag.
+     */
+    public Iterator<Item> iterator()  {
+        return new ListIterator();  
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next; 
+            return item;
+        }
+    }
+
+}
+
 
 interface Graph {
     public int V();
@@ -155,7 +243,7 @@ public String toString() {
  }
 
 
-public class Solution{
+public class Solution {
 
 	public static void main(String[] args) throws IOException {
 	
@@ -181,96 +269,6 @@ m1.display(s1, s2);
 		 break; 
 	 }
 	}
-
-}
-
-
-/*************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *
- *  A generic bag or multiset, implemented using a linked list.
- *
- *************************************************************************/
-
-
-
-/**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
- *  items in arbitrary order.
- *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- */
-class Bag<Item> implements Iterable<Item> {
-    private int N;         // number of elements in bag
-    private Node first;    // beginning of bag
-
-    // helper linked list class
-    private class Node {
-        private Item item;
-        private Node next;
-    }
-
-   /**
-     * Create an empty stack.
-     */
-    public Bag() {
-        first = null;
-        N = 0;
-    }
-
-   /**
-     * Is the BAG empty?
-     */
-    public boolean isEmpty() {
-        return first == null;
-    }
-
-   /**
-     * Return the number of items in the bag.
-     */
-    public int size() {
-        return N;
-    }
-
-   /**
-     * Add the item to the bag.
-     */
-    public void add(Item item) {
-        Node oldfirst = first;
-        first = new Node();
-        first.item = item;
-        first.next = oldfirst;
-        N++;
-    }
-
-
-   /**
-     * Return an iterator that iterates over the items in the bag.
-     */
-    public Iterator<Item> iterator()  {
-        return new ListIterator();  
-    }
-
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next; 
-            return item;
-        }
-    }
 
 }
 
